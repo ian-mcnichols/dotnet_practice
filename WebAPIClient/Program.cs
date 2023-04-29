0e58354d342c08713b80bee3a42096f2c9df61b1
Ian McNichols <ian.mcnichols@gmail.com>
2023-04-29 10:22:32 -0700

    Revert "Revert "Initial commit""
    This reverts commit b01d00660f7722c139539b9a22311111b89acc59.

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    await using Stream stream = await client.GetStreamAsync(
+        "https://api.github.com/orgs/dotnet/repos");
+    var respositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+
+    foreach (var repo in respositories ?? Enumerable.Empty<Repository>())
+        Console.Write(repo.name);
+}
