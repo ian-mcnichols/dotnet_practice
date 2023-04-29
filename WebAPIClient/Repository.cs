d9a88be65ed5edf9a5ffb2e7bcc06276bf643dd6
Ian McNichols <ian.mcnichols@gmail.com>
2023-04-29 10:38:54 -0700

    grabbing other properties

--- WebAPIClient/Repository.cs
+++ WebAPIClient/Repository.cs
@@ -1,4 +1,9 @@
 ﻿using System.Text.Json.Serialization;
 
 public record class Repository(
-    [property: JsonPropertyName("name")] string Name);
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("description")] string Description,
+    [property: JsonPropertyName("html_url")], Uri GitHubHomeUrl,
+    [property: JsonPropertyName("homepage")], Uri Homepage,
+    [property: JsonPropertyName("watchers")], int Watchers
+    );
