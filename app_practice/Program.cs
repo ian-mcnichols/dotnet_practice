3af2352a91246235e0d9d918fc7caf87e00c9359
Ian McNichols <ian.mcnichols@gmail.com>
2023-04-30 09:41:55 -0700

    Basic user interaction

--- app_practice/Program.cs
+++ app_practice/Program.cs
@@ -7,7 +7,35 @@ class Program
 {
         // entry-point method
     static void Main(string[] args) {
-        Console.WriteLine("Hello World");  
+            // setting variables
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Ian McNichols";
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}.");
+            Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}. Let's play a game.");
+
+            int correctNumber = 5;
+
+            Console.WriteLine("Guess a number from 1 to 10");
+            int guessNumber = Int32.Parse(Console.ReadLine());
+
+            while (guessNumber != correctNumber)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Incorrect guess. Try again! ");
+                Console.ResetColor();
+                guessNumber = Int32.Parse(Console.ReadLine());
+
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Correct! You won.");
     }
 }
 
