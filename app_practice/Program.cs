c8e475b35a0d485acd2655b0f4b52f503d7fde25
Ian McNichols <ian.mcnichols@gmail.com>
2023-04-30 09:19:39 -0700

    new practice

---
+++ app_practice/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace NumberGuesser
+{
+class Program
+{
+        // entry-point method
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World");  
+    }
+}
+
+}
