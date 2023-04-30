19ec206eb1f7eeeff307fe18fc53dca6a167086e
Ian McNichols <ian.mcnichols@gmail.com>
2023-04-30 10:50:03 -0700

    Moving to functions

--- app_practice/Program.cs
+++ app_practice/Program.cs
@@ -24,19 +24,43 @@ class Program
             int correctNumber = 5;
 
             Console.WriteLine("Guess a number from 1 to 10");
-            int guessNumber = Int32.Parse(Console.ReadLine());
+            int guessNumber = GetUserGuess();
 
             while (guessNumber != correctNumber)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Incorrect guess. Try again! ");
                 Console.ResetColor();
-                guessNumber = Int32.Parse(Console.ReadLine());
+                guessNumber = GetUserGuess();
 
             }
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Correct! You won.");
     }
+    static bool CheckInt(string input)
+    {
+        int guess;
+        if (int.TryParse(input, out guess) == true)
+        {
+            return true;
+        }
+        return false;         
+    }
+    static int GetUserGuess()
+        {
+            string userInput = Console.ReadLine();
+            bool isInt = CheckInt(userInput);
+            while (!isInt)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input. Must be integer guess.");
+                Console.ResetColor();
+                userInput = Console.ReadLine();
+                isInt = CheckInt(userInput);
+            }
+            int userGuess = Int32.Parse(userInput);
+            return userGuess;
+        }
 }
 
 }
